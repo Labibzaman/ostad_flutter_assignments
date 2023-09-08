905bd1682f57ebb08f225c82288f8d2f7e4279be
Labibuzzaman Labib <mdlabib315@gmail.com>
2023-09-08 23:13:14 +0600

    Updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:module_5_assignment_myapp/main.dart';
+import 'package:module_5_assignment_myapp/module_5_assignment.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
